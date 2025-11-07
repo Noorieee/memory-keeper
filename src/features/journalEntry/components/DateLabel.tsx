82011f40d62b0baba6783b442f4de2cab71b0114
Noorieee <hussainmahnoor112@gmail.com>
2025-11-07 20:42:19 +0000

    Styled initial date label

--- src/features/journalEntry/components/DateLabel.tsx
+++ src/features/journalEntry/components/DateLabel.tsx
@@ -1,11 +1,20 @@
 import styled from '@emotion/styled'
 import type { ColorKeys } from '../../../theme'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendar } from '@fortawesome/free-regular-svg-icons'
 
 const Container = styled.p<{ accentColor: ColorKeys }>(
   ({ theme, accentColor }) => {
     return `
+      width: fit-content;
       font-size: ${theme.fontSizes.sm}px;
+      color: ${theme.colors.text.dark};
       background-color: ${theme.colors[accentColor].main};
+      padding: ${theme.spacing.xs}px ${theme.spacing.sm}px;
+      border-radius: ${theme.spacing.lg}px; 
+      display: flex;
+      align-items: center;
+      gap: ${theme.spacing.xxs}px;
     `
   },
 )
@@ -16,7 +25,12 @@ interface DateLabelProps {
 }
 
 const DateLabel = ({ accentColor, children }: DateLabelProps) => {
-  return <Container accentColor={accentColor}>{children}</Container>
+  return (
+    <Container accentColor={accentColor}>
+      <FontAwesomeIcon icon={faCalendar} size="1x" />
+      {children}
+    </Container>
+  )
 }
 
 export default DateLabel
